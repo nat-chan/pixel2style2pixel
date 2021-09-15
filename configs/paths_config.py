fb6bc984c4397b68695b7a090e94f2ad353273df
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-09-15 20:48:40 +0900

    mocoの学習済み重みのpath、wandbをenvironmentに追加

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -18,5 +18,5 @@ model_paths = {
 	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
 	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
 	'shape_predictor': 'shape_predictor_68_face_landmarks.dat',
-	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pth.tar'
+	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pt'
 }
