de9b47410c3becaf12ddc3a999f219507de8d5d7
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-09-12 22:23:42 +0900

    出力にステータス欄を追加してQRの読み取り状態を表示。説明文を日本語化

--- script_gradio_app.py
+++ script_gradio_app.py
@@ -11,10 +11,9 @@ from options.test_options import TestOptions
 from scripts.inference import run_on_batch
 import torch
 from models.psp import pSp
-import os
-import sys
 import torchvision.transforms as transforms
 from nokogiri.working_dir import working_dir
+from datetime import datetime
 try:
     from script_imgtransform import imgtransform
 except:
@@ -62,9 +61,15 @@ def fn(content_image, style_image):
     content_image, style_image = imgtransform(content_image, style_image)
     try:
         seed = int(pyzbar.decode(Image.fromarray(style_image))[0].data)
+        if seed == 0:
+            output_text = f"Success, now random seed mode"
+        else:
+            output_text = f"Successfully read seed={seed} from QR"
     except:
-        print("# pyzbar.decode error")
         seed = 0
+        output_text = f"Failed to read QR, fall back to random seed"
+    now = datetime.now().strftime("%m/%d %H:%M %S")
+    print(now, output_text)
     
     content_torch = torch.from_numpy(
         sm.normalize(content_image[None,None,:,:]/255)
@@ -79,11 +84,13 @@ def fn(content_image, style_image):
     output_torch = run_on_batch(content_torch, net, opts, latent_to_inject)
     output_pillow = common.tensor2im(output_torch[0])
     output_numpy = np.array(output_pillow)
-    return output_numpy
+    return output_numpy, output_text
 
 
 content_image_input = gr.inputs.Image(label="スケッチの入力", shape=(512, 512), image_mode="L")
 style_image_input = gr.inputs.Image(label="塗り方の入力※左上のQRコードから読み取ります", shape=(512, 512))
+image_output = gr.outputs.Image(label="出力")
+status_output = gr.outputs.Textbox(label="ステータス")
 
 # Examples
 qr = list(map(str, Path("examples/qr").glob("*.png"))) # relative path
@@ -95,39 +102,38 @@ examples = [list(e) for e in zip(sim, qr)]
 iface = gr.Interface(
 fn=fn,
 inputs=[content_image_input, style_image_input],
-outputs="image",
+outputs=[image_output, status_output],
 examples=examples,
 examples_per_page=20,
 allow_flagging="never",
-allow_screenshot=True,
 title="スケッチからのキャラクタ生成",
 description="はじめに必ず下までスクロールして【使い方】をお読みください。",
 article="""
-# **【使い方】**
+## **【使い方】**
 
-- **Examples**から好きな<スケッチ, 塗り方>の組を選ぶ。
-- **Submit**ボタンを押す。
-- 右上の**OUTPUT**に実行経過時間が表示されるのでしばらく待つ。
-- **OUTPUT**コンポーネントに生成結果が表示される。
+- **入力例**から好きな<スケッチ, 塗り方>の組を選ぶ。
+- **送信**ボタンを押す。
+- 右上の**出力**に実行経過時間が表示されるのでしばらく待つ。
+- **出力**コンポーネントに生成結果が表示される。
 
-残念ながら**Examples**の各列をクリックしたとき、スケッチと塗り方がペアになっている例からしか選べないです。
-これはUIライブラリ側の都合によるもので、ペア以外の<スケッチ, 塗り方>の組で実行してみたい場合は以下の手順を踏んでください。
+**入力例**の各列をクリックすると、その列の<スケッチ, 塗り方>の両方が入力コンポーネントに登録されます。
+ペア以外の<スケッチ, 塗り方>の組で実行してみたい場合は以下の手順を踏んでください。
 
-- 好きな「塗り方の入力」を**Examples**から選び、クリックする。
-- 「スケッチの入力」コンポーネントにも画像がロードされるので**右上のXボタン**を押して消し、`Drop Image Here - or - Click to Upload`に切り替わる。
-- 好きな「スケッチの入力」を**Examples**から選び、左クリックで新しいタブで開く。
+- 好きな「塗り方の入力」を**入力例**から選び、クリックする。
+- 「スケッチの入力」コンポーネントにも画像がロードされるので**右上のXボタン**を押して消し、**ここに画像をドロップ-または-クリックしてアップロード**に切り替わる。
+- 好きな「スケッチの入力」を**入力例**から選び、左クリックで新しいタブで開く。
 - 新しいタブから「スケッチの入力」コンポーネントに選んだスケッチ画像をドラッグアンドドロップする。
-    - 同じタブを2つ開いて片方は入力コンポーネントを、もう片方は**Examples**を表示しドラッグアンドドロップするのも速いです。
+    - 同じタブを2つ開いて片方は入力コンポーネントを、もう片方は**入力例**を表示しドラッグアンドドロップするのも速いです。
 
-# **【自分で描いたスケッチで実行するやり方】**
+## **【自分で描いたスケッチで実行するやり方】**
 
-- まず適当なスケッチの入力を**Examples**から左クリック→名前をつけて画像を保存する。
+- まず適当なスケッチの入力を**入力例**から左クリック→名前をつけて画像を保存する。
 - ペイントソフトで**スケッチの線幅**を元のものと近いように修正・線を引く。
     - ※スケッチの線幅、色の濃さが元のものと離れると結果に反映されずらくなります。
-- 「スケッチの入力」コンポーネントの右上のXボタンを押して消し`Drop Image Here - or - Click to Upload`に切り替える。
+- 「スケッチの入力」コンポーネントの右上のXボタンを押して消し**ここに画像をドロップ-または-クリックしてアップロード**に切り替える。
 - 作成したスケッチをアップロードする。
 
-# **【画像の整列】**
+## **【画像の整列】**
 
 自分で描いたスケッチで実行したい方は顔の位置や大きさを揃えてください。
 学習データセットの平均を以下に示します。
@@ -143,10 +149,10 @@ article="""
   </tr>
 </table>
 
-# **【終わりに】**
+## **【終わりに】**
 使用している技術はStyleGAN2とpixel2style2pixelの組み合わせで特に新規なものではないです。
 また、ExamplesはStyleGAN2による生成画像と、そこから機械的に抽出されたスケッチです。
-これらの画像と生成画像の営利目的や非常識な使用はお控えください。
+これらの画像と生成画像の営利目的や非常識な使用はおやめください。
 今後の改善の為、フィードバックいただけると助かります。Regards,
 
 EOF
