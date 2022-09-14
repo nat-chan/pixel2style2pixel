48e075a761fb37cc80538c5508f2ece857e749bc
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-09-14 12:30:05 +0900

    mao様のqrを作成した時のをメモ代わりにデッドコードで残しておく

--- script_qr.py
+++ script_qr.py
@@ -5,7 +5,19 @@ import qrcode # 書き出し
 from pyzbar import pyzbar
 from PIL import Image
 import numpy as np
-# Examples
+
+# %%
+if False:
+    i = "/home/natsuki/mao_DBmiyuki/maoPR1.npz"
+    s = "/home/natsuki/mao_DBmiyuki/maoPR1.png"
+    bg = Image.open(s)
+    fg = qrcode.make(i)
+    fg = fg.resize((128, 128))
+    bg.paste(fg, (0, 0))
+    j = pyzbar.decode(bg)[0].data
+    assert i == j
+    bg.save("/home/natsuki/mao_DBmiyuki/maoQR1.png")
+# %%
 
 synth = list(Path("examples/synth").glob("*.png"))
 for s in synth:
