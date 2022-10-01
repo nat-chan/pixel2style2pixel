56a47601b43469fe52ce988e9bf81abf4110a097
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-10-01 13:48:15 +0900

    shuffle50のsurveyのhtml生成コード、codeのデリミタは|。データ容量を圧縮したjpgを用意したが権限の関係で読めなかったのでpngのまま

---
+++ web_shuffle50.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+import sys
+from typing import List
+from pathlib import Path
+import numpy as np
+
+def strip_seed(sname):
+    return int(sname.split("/")[-1].split(".")[0][4:])
+
+N = 50
+
+synth = sorted(map(str, Path("examples/synth").glob("*.png")))[:N]
+sim = sorted(map(str, Path("examples/sim").glob("*.png")))[:N]
+ref = sorted(map(str, Path("examples/synth").glob("*.png")))[N:]
+ver1 = [f"examples/sim_synth/{strip_seed(a)}_{strip_seed(b)}.png" for a, b in zip(sim, ref)]
+s2p = [f"examples/pair50/{i}/a.png" for i in range(N)]
+ver1_s2p = ver1 + s2p
+np.random.RandomState(0).shuffle(ver1_s2p)
+A, B = ver1_s2p[:N], ver1_s2p[N:]
+
+title = sys.argv[0].lstrip("web_").rstrip(".py")
+datasetlists: List[List[str]] = [A, B]
+names: List[str] = list()
+names = """
+左
+右
+""".strip().split("\n")
+
+jscode = """
+function onclick_button(i, j){
+	document.getElementById(`button_${i}_${j}`).style = 'background-color:cyan;';
+	document.getElementById(`button_${i}_${1-j}`).style = '';
+	document.getElementById(`select_${i}`).innerHTML = ['左', '右'][j];
+	let not_selected = [];
+	let code = "";
+	for(let k=0; k<50; k++){
+		let tmp_innerHTML = document.getElementById(`select_${k}`).innerHTML;
+		if(tmp_innerHTML != '左' && tmp_innerHTML != '右'){
+			not_selected.push(k);
+		}else{
+			code += +(tmp_innerHTML == '右');
+		}
+	}
+	if(not_selected.length == 0){
+		let encoded = "|";
+		for(let k=0; k<5; k++){
+			encoded += parseInt(code.substring(k*10, (k+1)*10), 2) + "|";
+		}
+		document.getElementById('input_top').value = encoded;
+	}else{
+		document.getElementById('input_top').value = '以下の番号がまだ未選択です'+not_selected.toString();
+	}
+	window.scrollBy(0,256);
+	console.log(i, j, code);
+}
+""".strip()
+
+th = '\n'.join(f"<th style='width:256px;height:50px;font-size:27px;'>{name}</th>" for name in names)
+print(f"""
+<!DOCTYPE html>
+<html>
+<head>
+<title>{sys.argv[0].rstrip(".py")}</title>
+<meta charset="utf-8"/>
+<script type="text/javascript">
+{jscode}
+</script>
+</head>
+<body>
+これからAIが生成した画像が2枚ずつ左と右に表示されます。<br>
+貴方は2枚を見比べてより美しいと思った方を選択してください。<br>
+この時、画像から何のAIが生成したか類推して選択するのはやめてください。<br>
+あくまで貴方の主観でより美しいと思った方を選択してください。<br>
+ファーストインプレッションが重要なので1枚につき10秒程度で選択ください。<br>
+全部で50組を選択し終えたら、トップに戻るボタンを押して以下の<br>
+テキストボックスに表示されるコードを私に教えることでアンケートは終了です<br>
+<br>
+<input id='input_top' style='width:512px;', value='まだ何も選択されていません'>
+</input>
+<table border=1 style='border: 1px solid black; border-collapse: collapse;'>
+<tr>
+<th>番号</th>
+<th>選択</th>
+{th}
+</tr>
+""")
+"""
+<input id='input_top' style='width:512px;height:256px;', value='まだ何も選択されていません'>
+"""
+for i, items in enumerate(zip(*datasetlists)):
+	print("<tr>")
+	print(f"<td>{i}</td>")
+	print(f"<td id='select_{i}'>未選択</td>")
+	for j, item in enumerate(items):
+		print("".join([
+			f"<td>",
+				f"<button id='button_{i}_{j}' onclick='onclick_button({i}, {j})' style=''>",
+					f"<img src='{item}' loading='lazy' style='width:256px;height:256px;'>",
+				f"</button>",
+			f"</td>"]))
+	print("</tr>")
+
+print("""
+</table>
+<button id='button_bottom' onclick='window.scrollTo(0, 0);' style='width:512px;height:256px;'>
+お疲れさまでした、トップに戻る
+</button>
+</body>
+</html>
+""")
