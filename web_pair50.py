0a64d6f2b38e8740d8ce468b2476426289839132
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-10-02 11:41:42 +0900

    pair50のclip美的スコア比較の実装

--- web_pair50.py
+++ web_pair50.py
@@ -3,6 +3,8 @@ import sys
 from glob import glob
 from typing import List
 from pathlib import Path
+import argparse
+import numpy as np
 
 def strip_seed(sname):
     return int(sname.split("/")[-1].split(".")[0][4:])
@@ -31,7 +33,8 @@ style2paints v4.5 w/ref
 assert len(datasetlists) == len(names)
 
 th = '\n'.join(f"<th style='width:256px;height:50px;font-size:27px;'>{name}</th>" for name in names)
-print(f"""
+HTML = list()
+HTML.append(f"""
 <!DOCTYPE html>
 <html>
 <head>
@@ -47,14 +50,34 @@ print(f"""
 """)
 
 for i, items in enumerate(zip(*datasetlists)):
-	print("<tr>")
-	print(f"<td>{i}</td>")
-	for item in items:
-		print(f"<td><img src='{item}' loading='lazy' style='width:256px;height:256px;'></td>")
-	print("</tr>")
+    HTML.append("<tr>")
+    HTML.append(f"<td>{i}</td>")
+    for item in items:
+        HTML.append(f"<td><img src='{item}' loading='lazy' style='width:256px;height:256px;'></td>")
+    HTML.append("</tr>")
 
-print("""
+HTML.append("""
 </table>
 </body>
 </html>
 """)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--readlink', type=str, default="")
+    parser.add_argument('--metrics', type=bool, default=False)
+    args = parser.parse_args()
+    if False: pass
+    elif args.metrics:
+        with open("examples/pair50_ver1_clip.txt", "r") as f:
+            ver1_clip = np.array([float(line.strip())for line in f.readlines()])
+        with open("examples/pair50_s2p_clip.txt", "r") as f:
+            s2p_clip = np.array([float(line.strip())for line in f.readlines()])
+        print(
+            100*(ver1_clip > s2p_clip).mean()
+        )
+    elif args.readlink != "":
+        for path in globals()[args.readlink]: # L or R
+            print(Path(path).absolute())
+    else:
+        print("\n".join(HTML))
